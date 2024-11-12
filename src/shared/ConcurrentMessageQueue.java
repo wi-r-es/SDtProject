767ff1c47b06a3d731d26ddd485f5adf216806fe
wrs <22085613+wi-r-es@users.noreply.github.com>
2024-11-12 02:08:29 +0000

    message and MessageQueue

---
+++ src/shared/ConcurrentMessageQueue.java
@@ -0,0 +1,25 @@
+package shared;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class ConcurrentMessageQueue {
+    private final ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
+
+    // method to add message to queue
+    public void enqueue(Message message) {
+        queue.add(message);
+        System.out.println("Enqueued: " + message);
+    }
+    // method to remove message from queue
+    public Message dequeue() {
+        Message message = queue.poll();
+        if (message != null) {
+            System.out.println("Dequeued: " + message);
+        }
+        return message;
+    }
+
+    // Method to check if the queue is empty
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+}
---
+++ src/shared/Message.java
@@ -0,0 +1,32 @@
+package shared;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Message {
+    private final int node_id;
+    private final AtomicInteger message_id = new AtomicInteger(0);
+    private final String message;
+    private final long timestamp;
+
+    public Message(int node_id, String message, long time){
+        this.node_id = node_id;
+        this.message=message;
+        this.timestamp = time;
+        message_id.getAndIncrement();
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public int getNodeId(){
+        return node_id;
+    }
+
+    public int getMessageId(){
+        return message_id.get();
+    }
+    public long getTimestamp(){
+        return this.timestamp;
+    }
+}
